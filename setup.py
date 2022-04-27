245e5df062da24de9d2b14782f1de0a27d84bb19
xgits <xgits@outlook.com>
2022-04-28 02:40:29 +0800

    solve opening file with encoding error for python 2.7

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="packagename",
     version="0.0.1",
     author="LIU Dian",
     author_email="xgits@outlook.com",
     description="pypi template",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
     project_urls={
         "Bug Tracker": "https://github.com/xgits/pypitemplate/issues",
     },
